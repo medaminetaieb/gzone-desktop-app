5f8f9069ca3accd3c3bddf93e9e26a39f0133250
bensalemiheb <99516071+bensalemiheb@users.noreply.github.com>
2022-02-28 22:47:12 +0100

    justinterface

---
+++ src/main/java/com/example/gzone/Forum1Controller.java
@@ -0,0 +1,34 @@
+package com.example.gzone;
+
+import java.awt.event.MouseEvent;
+import javafx.fxml.FXML;
+
+
+public class Forum1Controller {
+
+    @FXML
+    void Forum(MouseEvent event) {
+
+    }
+
+    @FXML
+    void HomePage(MouseEvent event) {
+
+    }
+
+    @FXML
+    void Store(MouseEvent event) {
+
+    }
+
+    @FXML
+    void Team(MouseEvent event) {
+
+    }
+
+    @FXML
+    void Tournament(MouseEvent event) {
+
+    }
+
+}
