094cb10d43953528c873aad43dbe98e24ddf08c8
chaymadhdhari <79309384+chaymadhdhari@users.noreply.github.com>
2022-03-10 10:48:02 +0100

    cc

--- src/main/java/com/example/gzone/HelloApplication.java
+++ src/main/java/com/example/gzone/HelloApplication.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("HomePage.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
